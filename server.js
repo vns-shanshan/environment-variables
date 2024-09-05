d7c6c09c981225c1a0ac0b2c8b728325129d3ea6
huijingj <vanessajin0426@gmail.com>
2024-09-04 18:03:38 -0700

    add environment variables

---
+++ server.js
@@ -0,0 +1,15 @@
+// This should be at very top
+require("dotenv").config();
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('The server is running');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+    console.log(`Your secret is ${process.env.SECRET_PASSWORD}`)
+});
